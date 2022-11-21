8024e9c8affdebe3d68f789e3717b259e0d32052
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2022-11-21 18:44:54 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import Search from "./componetns/Search";
 
-// import { useState, useEffect } from 'react'
-
 
 function App() {
 
-  // const [data1, setData] = useState();
-  // console.log(data1);
-  // const apikey = '700cd2716d1ea414ff0fe1483e04908c';
 
-  // useEffect(() => {
-  //   fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${'delhi'}&appid=${apikey}`).then(
-  //     (Response) => Response.json.main()
-
-  //   ).then(
-  //     (data1) => {
-  //       console.log(data1);
-  //       const arraydata = data1
-  //       setData(arraydata)
-  //     }
-  //   )
-  // }, [])
   return (
     <>
-    <Search></Search>
-   
+      <Search></Search>
 
     </>
-        );
+  );
 }
 
-        export default App;
+export default App;
--- src/componetns/Search.js
+++ src/componetns/Search.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 const Search = () => {
-    const [data, setData] = useState();
+    const [data, setData] = useState("mumbai");
     const [search, setSearch] = useState("mumbai");
 
     useEffect(() => {
@@ -25,27 +25,27 @@ const Search = () => {
                     <input type="search" placeholder="Search city name here" className="inputsearch" onChange={(event) => { setSearch(event.target.value) }}></input>
                 </div>
 
-                {!data ? (
-                <p>no data found</p>
-                ) : (
-                    <div>
-                    <div className="display">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdUijW8Pl6X57WSf5jgv0q00gtqFw0w0saJw&usqp=CAU" alt="" className="image1"></img>
-                        <h1>City-{search}</h1>
-                        <h1>{data.temp}°C</h1>
-                        <div className="minmax">
-                            <h2>Min-Temp <h4 > {data.temp_min}°C  </h4></h2>
-                
-                       
-                        <h2>Max-Temp<h4> {data.temp_max}°C </h4>  </h2>
-                        
-                        <h2>Humidity<h4>  {data.humidity}</h4></h2>
-                         
-                        </div>
-                       
+                {data ? (
+                 <div>
+                 <div className="display">
+                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdUijW8Pl6X57WSf5jgv0q00gtqFw0w0saJw&usqp=CAU" alt="" className="image1"></img>
+                     <h1>City-{search}</h1>
+                     <h1>{data.temp}°C</h1>
+                     <div className="minmax">
+                         <h2>Min-Temp <h4 > {data.temp_min}°C </h4></h2>
+             
+                    
+                     <h2>Max-Temp<h4> {data.temp_max}°C </h4>  </h2>
+                     
+                     <h2>Humidity<h4> {data.humidity}</h4></h2>
+                      
+                     </div>
+                    
 
-                    </div>
-                    </div>
+                 </div>
+                 </div>
+                ) : (
+                   <p>no data found</p>
                 )}
             </div>
 
